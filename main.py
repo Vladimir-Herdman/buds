61e2f86b3f055fca1a69943c16b8a0f4f75c44cd
Vladimir Herdman <vovaherdman@gmail.com>
2026-04-24 19:55:41 -0400

    Button works with sound effect

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import random
+
+import pygame
 import webview
 
 class MainApi:
     """Main bridge for frontend javascript to execute python."""
     def __init__(self):
         self._window: webview.Window|None = None
+        self.button_sound = pygame.mixer.Sound("vova-buhh.ogg")
 
     @property
     def window(self) -> webview.Window:
@@ -13,10 +17,15 @@ class MainApi:
     def window(self, win):
         self._window = win
 
-    def nullfunc(self, file:str) -> None:
-        print(f"nullfunc called from file:{file.split("/")[-1]}")
+    def play_sound(self):
+        self.button_sound.set_volume(random.choice([.7, .9, 1]))
+        self.button_sound.play()
+
+    def pyprint(self, s: str):
+        print("val:", s)
 
 def main():
+    pygame.mixer.init()
     api = MainApi()
 
     api.window = webview.create_window(
