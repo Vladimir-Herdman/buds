2087ee8410ad121090ee9f8b7fd9b84b3506e0a4
Vladimir Herdman <vovaherdman@gmail.com>
2026-04-27 14:59:36 -0400

    Made some developments and added Tyler sounds, adding output to be done

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
+import os
 import random
 
+from dataclasses import dataclass, fields
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 import pygame
 import webview
+
+
+@dataclass
+class ButtonInput:
+    num_office_hours: int
+    excel_file_path: str
+    unavailable_hours: str
+
+    def pyprint(self):
+        fieldvals = [_ for _ in fields(self)]
+        for fieldval in fieldvals:
+            val = getattr(self, fieldval.name)
+            if val == None: continue
+            print(f"{fieldval.name}: {val}-{type(val)}")
 
 class MainApi:
     """Main bridge for frontend javascript to execute python."""
     def __init__(self):
         self._window: webview.Window|None = None
-        self.button_sound = pygame.mixer.Sound("vova-buhh.ogg")
 
     @property
     def window(self) -> webview.Window:
@@ -17,11 +34,26 @@ class MainApi:
     def window(self, win):
         self._window = win
 
-    def play_sound(self):
-        self.button_sound.set_volume(random.choice([.7, .9, 1]))
-        self.button_sound.play()
+    def open_file_dialog(self):
+        file_types = ('Image Files (*.bmp;*.jpg;*.gif)', 'All files (*.*)')
+        result = self.window.create_file_dialog(
+            webview.FileDialog.OPEN, allow_multiple=True, file_types=file_types
+        )
+        return result;
 
-    def pyprint(self, s: str):
+    def _play_button_sound(self):
+        button_sound = pygame.mixer.Sound(random.choice(["vova-buhh.ogg", "tyler-buhh.ogg", "ollie-buhh.ogg", "jake-buhh.ogg"]))
+        button_sound.set_volume(random.choice([.7, .9, 1]))
+        button_sound.play()
+
+    def button_pressed(self, input: list):
+        self._play_button_sound();
+        if len(input) != len(fields(ButtonInput)): return
+
+        button_input = ButtonInput(*input)
+        button_input.pyprint()
+
+    def pyprint(self, s):
         print("val:", s)
 
 def main():
@@ -36,6 +68,7 @@ def main():
     )
 
     webview.start()
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
